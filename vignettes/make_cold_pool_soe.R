33ca665db2a40bf1aea8c50d3570c1ad6132319a
Joseph Caracappa (NOAA) <57966543+jcaracappa1@users.noreply.github.com>
2025-12-30 11:09:51 -0500

    cold pool workflow SOE 2026

--- vignettes/make_cold_pool_soe.R
+++ vignettes/make_cold_pool_soe.R
@@ -6,22 +6,23 @@ EDAB.GLORYS::make_cold_pool_data(
                     input.files = glorys.files,
                     output.dir = 'X:/jcaracappa/glorys_my_soe/data/cold_pool/',
                     output.prefix = 'glorys_cold_pool_',
-                    cp.shp.file = here::here('data-raw','geometry','cold_pool_area_GLORYS.shp')
+                    cp.shp.file = here::here('data-raw','geometry','cold_pool_area_GLORYS.shp'),
+                    redo=T
 )
 
-glorys.data = readRDS(here::here('data','cold_pool','GLORYS','glorys_cold_pool_monthly_1993_2024.rds')) %>%
+glorys.data = readRDS('X:/jcaracappa/glorys_my_soe/data/cold_pool/glorys_cold_pool_monthly_1993_2025.rds') |> 
   dplyr::mutate(source = 'GLORYS')
-roms.data = readRDS(here::here('data','cold_pool','ROMS','roms_debiased_cold_pool_monthly_1959_1992.rds'))%>%
+roms.data = readRDS('X:/jcaracappa/glorys_soe/data/cold_pool/ROMS/roms_debiased_cold_pool_monthly_1959_1992.rds') |> 
   dplyr::mutate(source = 'ROMS')
 input.data = dplyr::bind_rows(glorys.data,roms.data)
 rm(glorys.data,roms.data)
 
 make_cold_pool_extent(input.data = input.data,
-                      output.dir = here::here('data','cold_pool','/'),
+                      output.dir = 'V:/',
                       shp.file = here::here('data-raw','geometry','cold_pool_area.shp'),
                       grid.file = here::here('data','cold_pool','roms_cold_pool_grid.nc')
 )
 make_cold_pool_indices(input.data = input.data,
-                       cp.extent.file = here::here('data','cold_pool','cold_pool_maximum_extent_1959_2024.rds'),
-                       output.dir = here::here('data','cold_pool','/')
+                       cp.extent.file = 'V:/cold_pool_maximum_extent_1959_2025.rds',
+                       output.dir = 'V:/'
 )
